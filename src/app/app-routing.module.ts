427bb2450e0cd5bc638ef0787b927a2181235ca2
Conrado Campetella <conradocampetella@gmail.com>
2017-07-08 01:16:14 -0300

    commit with all the changes of the reorganization of the files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import{HomeComponent} from './home/home.component';
-import{WinesComponent} from './wines/wines.component';
-import{AboutComponent} from './about/about.component';
-import{ContactComponent} from './contact/contact.component';
-import { SignupComponent } from "app/auth/signup/signup.component";
-import { SigninComponent } from "app/auth/signin/signin.component";
-
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'wines', component: WinesComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'newuser', component: SignupComponent},
-  {path: 'login', component: SigninComponent}
+  {path: 'home', loadChildren: './home/home.module#HomeModule'}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { WinesComponent } from './wines/wines.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { WinesService } from './shared/wines.service';
-import { MapComponent } from './map/map.component';
-import { ContactformComponent } from './contact/contactform/contactform.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
+
+import {HomeModule} from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    FooterComponent,
-    WinesComponent,
-    AboutComponent,
-    ContactComponent,
-    MapComponent,
-    ContactformComponent,
-    SigninComponent,
-    SignupComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyC3e6U18NARu_LNFIb30O4IVZx0tVa5kjk'
-    }),
-    ReactiveFormsModule
+    HomeModule,
+    AppRoutingModule
   ],
-  providers: [WinesService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
